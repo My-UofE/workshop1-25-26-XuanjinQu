a337f86ebb9e0ab69037df8dabc8aeaf5d1eadc5
XuanjinQu <xq245@exeter.ac.uk>
2026-01-20 17:48:03 +0000

    game 3

--- game_functions.py
+++ game_functions.py
@@ -14,4 +14,15 @@ def check_higher_lower(current, next_num, guess):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    found = False
+    for i in range(len(word)):
+        if word[i] == letter:  # 
+            board[i] = letter   # 
+            found = True
+    if found :
+        print(f"{letter}")
+        return True
+    else:
+        print(f"{letter}")
+        return False
+
