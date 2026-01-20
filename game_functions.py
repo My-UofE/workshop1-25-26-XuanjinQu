5899281269dda9c5897c2607c91c016aeacea842
XuanjinQu <xq245@exeter.ac.uk>
2026-01-20 18:02:28 +0000

    game1

--- game_functions.py
+++ game_functions.py
@@ -1,9 +1,9 @@
 import random
 
 # function to be used by game_1: Guess the Number
-def pick_value(poss_values):
-    x = random.choice(poss_values)   
-    return x
+def pick_value(values):
+    return values[len(values) // 2]
+
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current, next_num, guess):
